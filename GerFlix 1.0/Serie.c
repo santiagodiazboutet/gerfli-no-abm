564a54e61eb008b1413a09cdab580c480ccc4462
santiagodiazboutet <santiagodiazboutet@gmail.com>
2018-04-27 19:47:30 -0300

    Add files via upload

---
+++ GerFlix 1.0/Serie.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include "Serie.h"
+
+/*
+    int idSerie;
+    char nombre[50];
+    char genero[20];
+    int cantidadTemporadas;
+    int estado;
+
+*/
+
+void inicializarSeriesEstado(eSerie series[], int cant)
+{
+    int i;
+
+    for(i=0; i<cant; i++)
+    {
+        series[i].estado = 0;
+    }
+}
+
+
+void inicializarSeriesHardCode(eSerie series[])
+{
+    int id[5] = {100,101,102,103,104};
+    char nombre[][50]={"TBBT","TWD","GOT","BB","LCDP"};
+    char genero[][50]={"Comedia","Terror","Suspenso","Policial","Policial"};
+    int cantidad[5] = {10,9,7,5,2};
+
+    int i;
+
+    for(i=0; i<5; i++)
+    {
+        series[i].idSerie=id[i];
+        series[i].cantidadTemporadas=cantidad[i];
+        series[i].estado = 1;
+        strcpy(series[i].nombre, nombre[i]);
+        strcpy(series[i].genero, genero[i]);
+    }
+}
+void mostrarListaSeries(eSerie series[], int cant)
+{
+    int i;
+    for(i=0;i<cant;i++)
+        {
+           if(series[i].estado==1)
+           {
+           printf("Serie: %s. Codigo: %d\n", series[i].nombre,series[i].idSerie);
+           }
+        }
+
+
+}
+
---
+++ GerFlix 1.0/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "UsuariosySeries.h"
+#define TAMSERIE 20
+#define TAMUSUARIO 100
+#define TAMUSUARIOSERIE 2000
+
+int main()
+{
+
+
+    int opcion=0;
+    int codigo=0;
+    eSerie listaDeSeries[TAMSERIE];
+    eUsuario listaDeUsuarios[TAMUSUARIO];
+    usuarioSerie listaUsuarioSerie[TAMUSUARIOSERIE];
+
+    while (opcion!=9)
+    {
+        printf("\nMENU\n1-ALTA\n2-BAJA\n3-MODIFICAR\n4-SERIES CON USUARIOS\n5-USUARIOS CON SERIES\n6-VER SERIES DE USUARIO\n7-VER USUARIOS DE SERIES\n8-LA SERIE MAS POPULAR\n9-SALIR\n");
+        scanf("%d", &opcion);
+        switch(opcion){
+        case 1:
+    inicializarSeriesEstado(listaDeSeries, TAMSERIE);
+    inicializarSeriesHardCode(listaDeSeries);
+    inicializarUsuariosEstado(listaDeUsuarios,TAMUSUARIO);
+    inicializarUsuariosHardCode(listaDeUsuarios);
+    inicializarUsuarioySeries(listaUsuarioSerie,listaDeUsuarios,listaDeSeries);
+
+        break;
+        case 4:
+            mostrarSerieConUsuario(listaDeUsuarios, listaDeSeries, TAMUSUARIO, TAMSERIE,listaUsuarioSerie);
+            break;
+        case 5:
+            mostrarUsuarioConSerie(listaDeUsuarios, listaDeSeries, TAMUSUARIO, TAMSERIE,listaUsuarioSerie);
+            ;
+        case 6:
+            mostrarListaUsuarios(listaDeUsuarios, TAMUSUARIO);
+            printf("\nIngrese el codigo del usuario que desea observar: \n");
+            scanf("%d",&codigo);
+            mostrarSeriesUsuario(listaDeSeries,listaDeUsuarios,listaUsuarioSerie,codigo);
+            break;
+        case 7:
+            mostrarListaSeries(listaDeSeries, TAMSERIE);
+            printf("\nIngrese el codigo de la serie que desea observar: \n");
+            scanf("%d",&codigo);
+            mostrarUsuariosSerie(listaDeSeries,listaDeUsuarios,listaUsuarioSerie,codigo);
+            break;
+        case 9:
+            break;
+
+        }
+    }
+
+
+
+
+
+
+    //mostrarListaSeries(listaDeSeries, TAMSERIE);
+    //mostrarListaUsuarios(listaDeUsuarios, TAMUSUARIO);
+
+
+
+
+
+    return 0;
+}
